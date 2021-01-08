b35e7adb042a39398814b0d6ec3b0a31043d7b79
anindamaulik <anindameister0@gmail.com>
2021-01-08 15:34:52 +0530

    pushing code

--- Nov30/src/main/java/com/emse/spring/faircorpagain/telusko/DemoApplication.java
+++ Nov30/src/main/java/com/emse/spring/faircorpagain/telusko/DemoApplication.java
@@ -1,25 +1,25 @@
-package com.emse.spring.faircorpagain.telusko;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-
-@SpringBootApplication
-public class DemoApplication {
-    public static void main(String[] args) {
-
-//        ConfigurableApplicationContext context=SpringApplication.run(DemoApplication.class, args);
+//package com.emse.spring.faircorpagain.telusko;
 //
-//            Alien alienObject=context.getBean(Alien.class);
-//        Alien alienObjects=context.getBean(Alien.class);
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.ConfigurableApplicationContext;
+//import org.springframework.context.annotation.ComponentScan;
 //
-//        alienObject.show();
-//        alienObjects.show();
-
-        SpringApplication.run(DemoApplication.class, args);
-
-    }
-}
-
+//@SpringBootApplication
+//public class DemoApplication {
+//    public static void main(String[] args) {
+//
+////        ConfigurableApplicationContext context=SpringApplication.run(DemoApplication.class, args);
+////
+////            Alien alienObject=context.getBean(Alien.class);
+////        Alien alienObjects=context.getBean(Alien.class);
+////
+////        alienObject.show();
+////        alienObjects.show();
+//
+//        SpringApplication.run(DemoApplication.class, args);
+//
+//    }
+//}
+//
